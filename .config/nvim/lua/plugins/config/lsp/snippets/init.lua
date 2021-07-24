8dc9277e81b1a5bea1db9b93f5dd975f4ea3186a
Praveen Dhawan <praveen@pdhawan.com>
2021-07-24 19:49:00 +0530

    Fix snippets - working fully

--- .config/nvim/lua/plugins/config/lsp/snippets/init.lua
+++ .config/nvim/lua/plugins/config/lsp/snippets/init.lua
@@ -15,145 +15,163 @@ local c = ls.choice_node
 ls.snippets = {
   all = {
     -- todo
-    s({ trig = "todo" }, t("TODO: ")),
-    s({ trig = "fixme" }, t("FIXME: ")),
-    s({ trig = "note" }, t("NOTE: "))
+    s({ trig = "todo" }, { t("TODO: ") }),
+    s({ trig = "fixme" }, { t("FIXME: ") }),
+    s({ trig = "note" }, { t("NOTE: ") })
   },
   lua = {
     -- function
-    parser(
-      { trig = 'fun' },
-      [[
-        function $1()
-          $0
-        end
-      ]]
+    s({ trig = 'fun' }, {
+        t('function '),
+        i(1, 'function_name'),
+        t({ '()', '  ' }),
+        i(0),
+        t({ '', 'end' })
+      }
     )
   },
   ruby = {
-    s({ trig = 'fsl' }, t("# frozen_string_literal: true")),
+    s({ trig = 'fsl' }, { t("# frozen_string_literal: true") }),
     -- all? { |JP1| JP2 }
-    s({ trig = 'all' },
-      t("all? { |"), i(1),
-      t("| "), i(0),
-      t("}")
+    s({ trig = 'all' }, {
+        t("all? { |"), i(1, 'block_args'),
+        t("| "), i(0),
+        t("}")
+      }
     ),
     -- any? { |JP1| JP2 }
-    s({ trig = 'any' },
-      t("any? { |"), i(1),
-      t("| "), i(0),
-      t("}")
+    s({ trig = 'any' }, {
+        t("any? { |"), i(1, 'block_args'),
+        t("| "), i(0),
+        t("}")
+      }
     ),
     -- map { |JP1| JP2 }
-    s({ trig = 'map' },
-      t("map { |"), i(1),
-      t("| "), i(0),
-      t("}")
+    s({ trig = 'map' }, {
+        t("map { |"), i(1, 'block_args'),
+        t("| "), i(0),
+        t("}")
+      }
     ),
     -- alias_method :JP1, :JP2
-    s({ trig = 'ali' },
-      t("alias_method :"), i(1, "new_name"),
-      t(", :"), i(0, "old_name")
+    s({ trig = 'ali' }, {
+        t("alias_method :"), i(1, "new_name"),
+        t(", :"), i(2, 'old_name'), i(0)
+      }
     ),
     -- attr_reader :JP1
-    s({ trig = 'attrr' },
-      t("attr_reader :"), i(1, "attribute_name")
+    s({ trig = 'attrr' }, {
+        t("attr_reader :"), i(1, "attribute_name"), i(0)
+      }
     ),
     -- attr_accessor :JP1
-    s({ trig = 'attra' },
-      t("attr_accessor :"), i(1, "attribute_name")
+    s({ trig = 'attra' }, {
+        t("attr_accessor :"), i(1, "attribute_name"), i(0)
+      }
     ),
     -- attr_accessor :JP1
-    s({ trig = 'attrw' },
-      t("attr_writer :"), i(1, "attribute_name")
+    s({ trig = 'attrw' }, {
+        t("attr_writer :"), i(1, "attribute_name"), i(0)
+      }
     ),
     -- require 'JP1'
-    s({ trig = 'req' },
-      t("require '"), i(1, "module_name"), t("'")
+    s({ trig = 'req' }, {
+        t("require '"), i(1, "module_name"), t("'"), i(0)
+      }
     ),
     -- require_relative 'JP1'
-    s({ trig = 'reqr' },
-      t("require_relative '"), i(1, "module_name"), t("'")
+    s({ trig = 'reqr' }, {
+        t("require_relative '"), i(1, "module_name"), t("'"), i(0)
+      }
     ),
     -- Connection execute
-    s({ trig = 'conne' },
-      t("ActiveRecord::Base.connection."),
-      c(2,
-        t("execute("),
-        t("select_values("),
-        t("select_rows("),
-        t("select_value(")
-      ),
-      i(1, "raw_sql_string"),
-      t(")")
+    s({ trig = 'conne' }, {
+        t("ActiveRecord::Base.connection."),
+        c(1, {
+          t("execute("),
+          t("select_values("),
+          t("select_rows("),
+          t("select_value(")
+        }),
+        i(2, "raw_sql_string"),
+        t(")"),
+        i(0)
+      }
     ),
     -- Transaction
-    s({ trig = 'trans'},
-      t("ActiveRecord::Base.transaction do\n  "),
-      i(0),
-      t("end")
+    s({ trig = 'trans'}, {
+        t("ActiveRecord::Base.transaction do", '  '),
+        i(0),
+        t("end")
+      }
     ),
     -- Class
-    s({ trig = 'cla'},
-      t("class "),
-      i(2, "ClassName"),
-      t("\n  def initialize("),
-      i(1, 'args'),
-      t(')\n    '),
-      i(0, 'body'),
-      t('  end\nend')
+    s({ trig = 'cla'}, {
+        t("class "),
+        i(1, "ClassName"),
+        t('', "  def initialize("),
+        i(2, 'args'),
+        t(')', '    '),
+        i(0),
+        t('  end', 'end')
+      }
     ),
     -- Module
-    s({ trig = 'mod'},
-      t("module "),
-      i(3, "ModuleName"),
-      t("\n  def "),
-      i(2, "method_name"),
-      t('('),
-      i(1, 'args'),
-      t(')\n    '),
-      i(0, 'body'),
-      t('  end\nend')
+    s({ trig = 'mod'}, {
+        t("module "),
+        i(1, "ModuleName"),
+        t('', "  def "),
+        i(2, "method_name"),
+        t('('),
+        i(3, 'args'),
+        t(')', '    '),
+        i(0),
+        t('  end', 'end')
+      }
     ),
     -- def method
-    s({ trig = 'def'},
-      t("def "),
-      i(2, "method_name"),
-      t('('),
-      i(1, 'args'),
-      t(')\n  '),
-      i(0, 'body'),
-      t('  end')
+    s({ trig = 'def'}, {
+        t("def "),
+        i(1, 'method_name'),
+        t('('),
+        i(2, 'args'),
+        t({')', '  '}),
+        i(0),
+        t({'', 'end'})
+      }
     ),
     -- progress bar
-    s({ trig = 'bar'},
-      t("bar = ProgressBar.new("),
-      i(0, 'count'),
-      t(')')
+    s({ trig = 'bar'}, {
+        t("bar = ProgressBar.new("),
+        i(0, 'count'),
+        t(')')
+      }
     ),
   },
   css = {
-    parser({ trig = 'pad', name = 'padding'}, "padding: ${0:0};"),
-    parser({ trig = 'mar', name = 'margin'}, "margin: ${0:0};"),
-    parser({ trig = 'bor', name = 'border'}, "border: ${0:0};"),
-    parser({ trig = 'dis', name = 'display'}, "display: ${0:none};"),
-    parser({ trig = 'back', name = 'background'}, "background: ${0:none};"),
-    parser({ trig = 'fon', name = 'font-family'}, "font-family: ${1:'Arial'};"),
-    parser({ trig = 'hei', name = 'height'}, "height: ${0:auto};"),
-    parser({ trig = 'wid', name = 'width'}, "width: ${0:auto};"),
-    parser({ trig = 'pos', name = 'position'}, "position: ${0:relative};"),
-    parser({ trig = 'imp', name = '!important'}, "!important $0"),
+    s({ trig = 'pad', name = 'padding'}, { t("padding: "), i(1, '0'), t(';'), i(0) }),
+    s({ trig = 'mar', name = 'margin'}, { t("margin: "), i(1, '0'), t(';'), i(0) }),
+    s({ trig = 'bor', name = 'border'}, { t("border: "), i(1, '0'), t(';'), i(0) }),
+    s({ trig = 'dis', name = 'display'}, { t("display: "), i(1, 'none'), t(';'), i(0) }),
+    s({ trig = 'bg', name = 'background'}, { t("background: "), i(1, 'none'), t(';'), i(0) }),
+    s({ trig = 'fon', name = 'font-family'}, { t("font-family: "), i(1, 'Arial'), t(';'), i(0) }),
+    s({ trig = 'hei', name = 'height'}, { t("height: "), i(1, 'auto'), t(';'), i(0) }),
+    s({ trig = 'wid', name = 'width'}, { t("width: "), i(1, 'auto'), t(';'), i(0) }),
+    s({ trig = 'pos', name = 'position'}, { t("position: "), i(1, 'relative'), t(';'), i(0) }),
+    s({ trig = 'imp', name = 'important'}, { t("!important: "), i(0) }),
   },
   javascript = {
-    parser({ trig = 'con', name = 'console'}, "console.log(${0:'hi'});"),
-    parser({ trig = 'log', name = 'console'}, "console.log(${0:'hi'});"),
-    parser({ trig = 'ale', name = 'alert'}, "alert(${0:'hi'});"),
-    parser({ trig = 'func', name = 'function'},
-      [[
-        function ${2:function_name} (${1:function_args}) {
-          $0
-        }
-      ]]
-    ),
+    s({ trig = 'con', name = 'log'}, { t('console.log('), i(1, 'hi'), t(');') }),
+    s({ trig = 'log', name = 'log'}, { t('console.log('), i(1, 'hi'), t(');') }),
+    s({ trig = 'ale', name = 'alert'}, { t('alert('), i(1, 'hi'), t(');') }),
+    s({ trig = 'func', name = 'function'}, {
+      t('function ('),
+      i(1, 'function_name'),
+      t('('),
+      i(2, 'function_args'),
+      t(') {', '  '),
+      i(0),
+      t('}')
+    }),
   }
 }
