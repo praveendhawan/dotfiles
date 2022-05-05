804a5066d2ec2013efc9478f91b7ba3e2d51daad
Praveen Dhawan <praveen@pdhawan.com>
2022-05-05 11:02:57 +0530

    Fix cmpletion after update, add more sources, add source name in completion menu

--- .config/nvim/lua/plugins/config/cmp/init.lua
+++ .config/nvim/lua/plugins/config/cmp/init.lua
@@ -18,10 +18,14 @@ cmp.setup {
     keyword_length = 3
   },
   mapping = {
-    ["<c-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ["Up"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ["<c-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-    ["Down"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    -- ["<c-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<c-p>"] = cmp.mapping(cmp.mapping.select_next_item(), {'i','c'}),
+    ["Up"] = cmp.mapping(cmp.mapping.select_next_item(), {'i','c'}),
+    -- ["Up"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    -- ["<c-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<c-n>"] = cmp.mapping(cmp.mapping.select_prev_item(), {'i','c'}),
+    -- ["Down"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["Down"] = cmp.mapping(cmp.mapping.select_prev_item(), {'i','c'}),
     ["<c-d>"] = cmp.mapping.scroll_docs(-4),
     ["<c-f>"] = cmp.mapping.scroll_docs(4),
     ["<c-space>"] = cmp.mapping.complete(),
@@ -29,7 +33,22 @@ cmp.setup {
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
   },
   formatting = {
-    format = lspkind.cmp_format(),
+    format = lspkind.cmp_format({
+      mode = 'symbol_text',
+      menu = ({
+        buffer                   = "[B]",
+        cmdline                  = "[Cmd]",
+        copilot                  = "[Cop]",
+        luasnip                  = "[Snip]",
+        nvim_lsp                 = "[LSP]",
+        nvim_lsp_document_symbol = "[LSP-DS]",
+        nvim_lsp_signature_help  = "[Sig]",
+        nvim_lua                 = "[Lua]",
+        path                     = "[P]",
+        tags                     = "[Tag]",
+        treesitter               = "[TS]",
+      })
+    }),
   },
   sources = {
     { name = 'path', priority = 1 },
@@ -37,9 +56,37 @@ cmp.setup {
     { name = 'luasnip', priority = 2 },
     { name = 'treesitter', priority = 3 },
     { name = 'nvim_lsp', priority = 4 },
+    { name = 'copilot', priority = 5 },
+    { name = 'nvim_lsp_signature_help', priority = 6 },
     -- { name = 'buffer' },
-    { name = 'tags', priority = 5, max_item_count = 5 },
+    { name = 'tags', priority = 5, max_item_count = 7 },
     -- { name = 'spell' },
-    { name = 'nvim_lua', priority = 6 },
   }
 }
+
+--  Enable nvim lua source only for lua files
+cmp.setup.filetype('lua', {
+  sources = cmp.config.sources({
+    { name = 'nvim_lua', priority = 8 },
+  })
+})
+
+-- Setup for vim search using /
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'nvim_lsp_document_symbol' },
+    { name = 'buffer' }
+  }
+})
+
+-- Setup for CMD mode using ':'
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
