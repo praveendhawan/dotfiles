85c67aab8a4ef06f5505f3732dd8e9c1000b93c6
Praveen Dhawan <praveen@pdhawan.com>
2022-05-10 14:00:22 +0530

    Add desc for keybindings

--- .config/nvim/lua/plugins/keybindings/harpoon.lua
+++ .config/nvim/lua/plugins/keybindings/harpoon.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_set_keymap('n', '<leader>h', '', { callback = require('harpoon.mark').add_file, desc = 'Add file to harpoon' })
 -- Currently assigned to Gitsigns unstage hunk
 -- vim.api.nvim_set_keymap('n', '<leader>hu', "<cmd> lua require('harpoon.ui').toggle_quick_menu()<cr>", noremap)
-vim.api.nvim_set_keymap('n', '<leader>hnn', '', { callback = require('harpoon.ui').nav_next })
-vim.api.nvim_set_keymap('n', '<leader>hpp', '', { callback = require('harpoon.ui').nav_prev })
+vim.api.nvim_set_keymap('n', '<leader>hnn', '', { callback = require('harpoon.ui').nav_next, desc = 'Goto Next Mark' })
+vim.api.nvim_set_keymap('n', '<leader>hpp', '', { callback = require('harpoon.ui').nav_prev, desc = 'Goto Previous Mark' })
 
