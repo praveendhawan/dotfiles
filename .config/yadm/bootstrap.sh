8d1e1d84119bbd0c48666ceca3745fa68dde3bcf
Praveen Dhawan <praveen@pdhawan.com>
2023-03-03 18:20:49 +0000

    Make bootstrap idempotent

--- .config/yadm/bootstrap.sh
+++ .config/yadm/bootstrap.sh
@@ -1,17 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-# Symlink for fdfind
-ln -s fdfind /usr/local/bin/fd
+# Check if fd symlink already exists before creating
+if [ ! -L /usr/local/bin/fd ]; then
+  ln -s "$(which fdfind)" /usr/local/bin/fd
+fi
 
-# Install awscli
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+# Check if AWS CLI v2 is already installed before attempting to install
+if ! command -v aws &> /dev/null; then
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install
+fi
 
-# Install ctree
-git clone git@github.com:shkrt/component_tree.git
-sudo cp -R component_tree/ /opt/
-sudo chmod a+x /opt/component_tree/bin/ctree
-export PATH="$PATH:/opt/component_tree/bin"
-source ~/.zshrc
-
+# Check if ctree is already installed before attempting to install
+if [ ! -d /opt/component_tree ]; then
+  git clone git@github.com:shkrt/component_tree.git
+  sudo cp -R component_tree/ /opt/
+  sudo chmod a+x /opt/component_tree/bin/ctree
+  export PATH="$PATH:/opt/component_tree/bin"
+fi
