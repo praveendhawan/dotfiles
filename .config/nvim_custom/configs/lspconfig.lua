dfbb17570ee865373388ee7cc2eb5cf6e2c1c80b
Praveen Dhawan <praveen@pdhawan.com>
2023-07-05 11:39:31 +0530

    Add solargraph config for LSP

--- .config/nvim_custom/configs/lspconfig.lua
+++ .config/nvim_custom/configs/lspconfig.lua
@@ -15,3 +15,35 @@ end
 
 -- 
 -- lspconfig.pyright.setup { blabla}
+-- Ruby Server
+
+local server_options = {
+  capabilities = capabilities,
+  flags = {
+    debounce_text_changes = 150,
+  },
+  init_options = {
+    formatting = false
+  },
+  on_attach = on_attach,
+  settings = {
+    solargraph = {
+      autoformat = false,
+      checkGemVersion = true,
+      completion = true,
+      definitions = true,
+      diagnostics = true,
+      folding = false,
+      formatting = false,
+      hover = true,
+      logLevel = 'warn',
+      references = true,
+      rename = true,
+      symbols = true,
+      transport = 'stdio',
+      useBundler = false
+    }
+  }
+}
+
+lspconfig.solargraph.setup(server_options)
