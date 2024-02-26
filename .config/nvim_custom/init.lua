7de3f53c5c41b6ed612791d24bc430f3c0256f12
Praveen Dhawan <praveen@pdhawan.com>
2024-02-26 09:23:05 +0530

    Fix wrong indentation for Treesitter

--- .config/nvim_custom/init.lua
+++ .config/nvim_custom/init.lua
@@ -11,3 +11,5 @@ vim.g.lua_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/snippets"
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = false
+
+vim.cmd [[autocmd FileType ruby setlocal indentkeys-=.]]
