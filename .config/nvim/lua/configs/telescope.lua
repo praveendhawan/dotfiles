c5a0eea28fd4c72bbd7089359f9aab8e18d6fc97
Praveen Dhawan <praveen@pdhawan.com>
2024-05-02 14:16:08 +0530

    FMT - Stylua Format

--- .config/nvim/lua/configs/telescope.lua
+++ .config/nvim/lua/configs/telescope.lua
@@ -1,15 +1,15 @@
 return {
   defaults = {
     vimgrep_arguments = {
-      'rg',
-      '-L',
-      '--color=never',
-      '--no-heading',
+      "rg",
+      "-L",
+      "--color=never",
+      "--no-heading",
       "--with-filename",
-      '--line-number',
-      '--column',
-      '--smart-case',
-      '-H'
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "-H",
     },
     file_ignore_patterns = {
       "node_modules/.*",
@@ -19,7 +19,7 @@ return {
       "public/.*",
       "vendor/.*",
       "docs/.*",
-      '*.log',
+      "*.log",
       "storage/.*",
       "tags/.*",
     },
@@ -28,37 +28,16 @@ return {
     find_files = {
       previewer = false,
       wrap_results = true,
-      path_display = { shorten = { len = 4 } }
+      path_display = { shorten = { len = 4 } },
     },
-    -- current_buffer_fuzzy_find = {
-    --   theme = 'dropdown'
-    -- },
-    -- live_grep = {
-    --   theme = 'dropdown'
-    -- },
-    -- buffers = {
-    --   previewer = false,
-    --   theme = 'dropdown'
-    -- },
-    -- help_tags = {
-    --   theme = 'dropdown'
-    -- },
-    -- grep_string = {
-    --   theme = 'dropdown'
-    -- },
-    -- keymaps = {
-    --   theme = 'dropdown',
-    --   previewer = false
-    -- }
   },
   extensions_list = { "themes", "terms", "fzf", "ui-select" },
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-    }
-  }
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+    },
+  },
 }
-
