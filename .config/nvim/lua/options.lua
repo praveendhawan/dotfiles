3300df40ad5019e5fdb47bdbeca493caf6acaeb6
Praveen Dhawan <praveen@pdhawan.com>
2024-05-15 15:35:54 +0530

    Remove code to run on ruby provider as it is not required anymore
    I added this as I thought it will somehow help with installation
    error of solargraph but it didnt.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,14 +6,3 @@ local o = vim.o
 local g = vim.g
 o.cursorlineopt = "both" -- to enable cursorline!
 g.lua_snippets_path = vim.fn.stdpath "config" .. "/lua/configs/snippets"
-
--- Need to enable Ruby as it is off by default in Nvchad
--- https://github.com/NvChad/NvChad/issues/1417#issuecomment-1203490842
-local enable_providers = {
-  "ruby_provider",
-}
-
-for _, plugin in pairs(enable_providers) do
-  g["loaded_" .. plugin] = nil
-  vim.cmd("runtime " .. plugin)
-end
