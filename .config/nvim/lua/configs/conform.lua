ea7bc1e41e54443fc21d0d76efc09ba1b9bfe812
Praveen Dhawan <praveen@pdhawan.com>
2024-05-15 15:35:26 +0530

    Switch off rubocop auto format on save

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -3,7 +3,7 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    ruby = { "rubocop" },
+    -- ruby = { "rubocop" },
     ["*"] = { "trim_whitespace" },
   },
 
