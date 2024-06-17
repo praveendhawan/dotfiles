464e53d6054ba317c8c61fa4faaa91b2628daa23
Praveen Dhawan <praveen@pdhawan.com>
2024-06-18 02:57:39 +0530

    enabling these made nvim slow in quitting and closing the nvim tree

--- .config/nvim/lua/configs/nvimtree.lua
+++ .config/nvim/lua/configs/nvimtree.lua
@@ -1,39 +1,48 @@
+local ignore_dirs = {
+  "^.bundle",
+  "^build",
+  "^storage",
+  "^.yardoc",
+  "^node_modules",
+  "^cache",
+  "^pids",
+  "^sockets",
+  "^public",
+  "^vendor",
+  "^.git",
+  "^.github",
+  "^.dockerdev",
+  "^docs",
+  "^log",
+  "^.sass-cache",
+}
 return {
   git = {
     enable = true,
   },
-  modified = {
-    enable = true,
-    show_on_dirs = true,
-    show_on_open_dirs = true
-  },
-  filters = {
-    enable = true,
-    git_ignored = false,
-    dotfiles = false,
-    custom = {
-      "node_modules",
-      "cache",
-      "pids",
-      "sockets",
-      "public",
-      "vendor",
-      ".git",
-      ".github",
-      ".dockerdev",
-      "docs",
-      "log"
-    }
-  },
+  -- modified = {
+  --   enable = true,
+  --   show_on_dirs = true,
+  --   show_on_open_dirs = true,
+  -- },
+  -- filters = {
+  --   enable = true,
+  --   git_ignored = false,
+  --   dotfiles = false,
+  --   custom = ignore_dirs,
+  -- },
   renderer = {
-    add_trailing = true,
-    highlight_modified = "icon",
+    -- add_trailing = true,
+    -- highlight_modified = "icon",
     highlight_git = "icon",
     icons = {
       show = {
         git = true,
-        modified = true
+        -- modified = true,
       },
     },
   },
+  -- filesystem_watchers = {
+  -- ignore_dirs = ignore_dirs,
+  -- },
 }
