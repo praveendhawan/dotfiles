712d0f91d44a48995b282e151e4623d0f6ada609
Praveen Dhawan <praveen@pdhawan.com>
2024-06-17 10:59:57 +0530

    Add more options for nvimtree and other improvements

--- .config/nvim/lua/configs/nvimtree.lua
+++ .config/nvim/lua/configs/nvimtree.lua
@@ -2,12 +2,37 @@ return {
   git = {
     enable = true,
   },
-
+  modified = {
+    enable = true,
+    show_on_dirs = true,
+    show_on_open_dirs = true
+  },
+  filters = {
+    enable = true,
+    git_ignored = false,
+    dotfiles = false,
+    custom = {
+      "node_modules",
+      "cache",
+      "pids",
+      "sockets",
+      "public",
+      "vendor",
+      ".git",
+      ".github",
+      ".dockerdev",
+      "docs",
+      "log"
+    }
+  },
   renderer = {
-    highlight_git = true,
+    add_trailing = true,
+    highlight_modified = "icon",
+    highlight_git = "icon",
     icons = {
       show = {
         git = true,
+        modified = true
       },
     },
   },
