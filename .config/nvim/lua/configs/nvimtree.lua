9c484b89afaf1e62d60e694b69ffeb10fc6e5032
Praveen Dhawan <praveen@pdhawan.com>
2025-02-10 17:12:57 +0500

    update nvimtree config to not use highlight git

--- .config/nvim/lua/configs/nvimtree.lua
+++ .config/nvim/lua/configs/nvimtree.lua
@@ -34,7 +34,7 @@ return {
   renderer = {
     -- add_trailing = true,
     -- highlight_modified = "icon",
-    highlight_git = "icon",
+    -- highlight_git = "icon",
     icons = {
       show = {
         git = true,
